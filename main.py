5e0a1fefcdd39dae4e4b452219c7ee328fad275a
parthsavla <parthsavla619@gmail.com>
2022-05-27 16:27:48 +0300

    with better script handling

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ print('Select the interface from the above list. (Shows available interfaces)')
 interface = input("Select the interface > ")
 new_mac = input("the new mac address > ")
 
-print("Changing make address for interface " + interface + " to " + new_mac)
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+
+
+subprocess.call(["ifconfig",interface,"down"])
+subprocess.call(["ifconfig",interface,"hw","ether",new_mac])
+subprocess.call(["ifconfig",interface,"up"])
